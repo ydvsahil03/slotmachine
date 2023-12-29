9cf56e49c955e3001e2c595f58b6d3101c0612ad
ydvsahil03 <ydvsahil0003@gmail.com>
2023-12-29 20:25:50 +0530

    secon dragon

--- main.py
+++ main.py
@@ -16,11 +16,28 @@ symbol_count = {
 }
 
 
-def get_slot_machine_spin(ROWS, COLS, symbols):
+def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
-            all_symbols.append()
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+
+
 
 
 def deposit():
